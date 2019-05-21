f8a594f3df6ad9d4a9c91a9571bd88155b7bc0cc
Victoria Keating <vkeating@fusionalliance.com>
2019-05-21 11:05:38 -0400

    Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,47 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override void CalculateStatistics()
+        {
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            base.CalculateStudentStatistics(name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override double GetGPA(char letterGrade, StudentType studentType)
+        {
+            return base.GetGPA(letterGrade, studentType);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
