aecd4d5ece715445f5a7ae24a0ba1ddf6ff6741a
Victoria Keating <vkeating@fusionalliance.com>
2019-05-21 14:18:55 -0400

    Finished Override Statistics Module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,11 +38,22 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
+            if (Students.Count > 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+                return;
+            }
             base.CalculateStatistics();
         }
 
+        //This method does not check if there is a grade on file for each student
         public override void CalculateStudentStatistics(string name)
         {
+            if (Students.Count > 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
             base.CalculateStudentStatistics(name);
         }
 
